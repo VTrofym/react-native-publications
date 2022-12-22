7d5dff260403c6d95e3e22f42e886c08c4b25b98
VTrofym <v.trof@icloud.com>
2022-12-22 22:33:59 +0200

    added form

--- publications/App.js
+++ publications/App.js
@@ -1,11 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ImageBackground,
+  Platform,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Keyboard,
+} from "react-native";
 
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    // console.log("state", state);
+    // setState(initialState);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require("./assets/images/PhotoBG.jpg")}
+        style={styles.imageBG}
+      >
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "margin"}
+        >
+          <View
+            style={{
+              ...styles.form,
+              paddingBottom: isShowKeyboard ? 0 : 78,
+              // width: dimensions,
+            }}
+          >
+            <Text style={styles.title}>Реєстрація</Text>
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+              onFocus={() => setIsShowKeyboard(true)}
+            />
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+              onFocus={() => setIsShowKeyboard(true)}
+            />
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+              secureTextEntry={true}
+              onFocus={() => setIsShowKeyboard(true)}
+            />
+            <TouchableOpacity
+              style={styles.btn}
+              activeOpacity={0.6}
+              onPress={keyboardHide}
+            >
+              <Text style={styles.btnTitle}>Зареєструватися</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.btnQuestion}
+              activeOpacity={0.6}
+              onPress={keyboardHide}
+            >
+              <Text style={styles.btnQuestionText}>Вже є акаунт? Увійти!</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +78,65 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  imageBG: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  form: {
+    backgroundColor: "#FFFFFF",
+    // paddingBottom: 78,
+    paddingTop: 92,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 30,
+    lineHeight: 35,
+    color: "#212121",
+    marginBottom: 32,
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    height: 50,
+    marginBottom: 16,
+    marginHorizontal: 16,
+  },
+  btn: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    backgroundColor: Platform.OS === "ios" ? "#FF6C00" : "#FF6C00",
+    borderRadius: 25,
+    marginHorizontal: 16,
+    marginBottom: 16,
+    marginTop: 27,
+  },
+  btnTitle: {
+    lineHeight: 19,
+    ...Platform.select({
+      ios: {
+        color: "#FFFFFF",
+        fontSize: 16,
+      },
+      android: {
+        color: "#FFFFFF",
+        fontSize: 16,
+      },
+    }),
+  },
+  btnQuestion: {
+    alignItems: "center",
+  },
+  btnQuestionText: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
   },
 });
