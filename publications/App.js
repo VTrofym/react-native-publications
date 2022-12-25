f0480bba461233371d317b0dd66df4966c257150
VTrofym <v.trof@icloud.com>
2022-12-25 15:27:25 +0200

    added redux-toolkit

--- publications/App.js
+++ publications/App.js
@@ -1,7 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react";
+
 import { useRoute } from "./router";
+import { store } from "./redux/store";
 
 export default function App() {
   const routing = useRoute(true);
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    // <Provider store={store}>
+    <NavigationContainer>{routing}</NavigationContainer>
+    // </Provider>
+  );
 }
